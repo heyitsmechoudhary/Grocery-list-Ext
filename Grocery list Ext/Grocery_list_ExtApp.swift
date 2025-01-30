455f0cada9704828ec20d908c294f692b6d3c3c2
Rahul choudhary <heyitsmechoudhary@gmail.com>
2025-01-30 19:45:23 +0530

    Initial Commit

---
+++ Grocery list Ext/Grocery_list_ExtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grocery_list_ExtApp.swift
+//  Grocery list Ext
+//
+//  Created by Rahul choudhary on 30/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Grocery_list_ExtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
